1ad099d4df48b3cbd73a0343a3a5bf6efe73d9f7
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-22 08:23:35 -0700

    monty program

--- pall.c
+++ pall.c
@@ -3,14 +3,16 @@
 /**
 * pallnode - Prints all the values on the stack
 * @stack: Double pointer to the head of the stack
-*
+* @counter: unused
 * Description:
 * This function prints all the values stored in the stack,
 * starting from the top of the stack.
 */
-void pallnode(stack_t **stack)
+void pallnode(stack_t **stack, unsigned int counter)
 {
-stack_t *current = *stack;
+stack_t *current;
+(void)counter;
+current = *stack;
 while (current != NULL)
 {
 printf("%d\n", current->n);
