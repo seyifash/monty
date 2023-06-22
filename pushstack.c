ff5c036408fe8e3b33aae98f79fed95dc2b04f3c
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-22 10:18:35 -0700

    monty program

--- pushstack.c
+++ pushstack.c
@@ -1,45 +1,5 @@
 #include "monty.h"
 /**
-* is_integer - checks if a string is a valid integer
-* @str: string to check
-* Return: 1 if string is a valid integer, 0 otherwise
-*/
-int is_integer(char *str)
-{
-int i = 0;
-if (str[0] == '-')
-i++;
-for (; str[i] != '\0'; i++)
-{
-if (str[i] < '0' || str[i] > '9')
-return (0);
-}
-return (1);
-}
-/**
-* mpush - add node to the stack
-* @head: stack head
-* @counter: line number
-* Return: no return
-*/
-void mpush(stack_t **head, unsigned int counter)
-{
-int f = 0, n;
-if (shared.arg == NULL || !is_integer(shared.arg))
-{
-fprintf(stderr, "L%d: usage: push integer\n", counter);
-freestack(*head);
-exit(EXIT_FAILURE);
-}
-if (shared.arg[0] == '-')
-f++;
-n = atoi(shared.arg);
-if (shared.tflag == 0)
-pushnode(head, n);
-else
-pushqueue(head, n);
-}
-/**
 * mqueue - sets the stack mode to queue (FIFO)
 * @head: unused
 * @counter: unused
@@ -62,4 +22,44 @@ void mstack(stack_t **head, unsigned int counter)
 (void)head;
 (void)counter;
 shared.tflag = 0;
+}
+/**
+* pallnode - prints the stack
+* @head: stack head
+* @counter: unused
+* Return: no return
+*/
+void pallnode(stack_t **head, unsigned int counter)
+{
+stack_t *curr;
+(void)counter;
+curr = *head;
+while (curr != NULL)
+{
+printf("%d\n", curr->n);
+curr = curr->next;
+}
+}
+/**
+* pushnode - pushes a node to the top of the stack in a linked list
+* @head: the pointer to the hea node
+* @value: the dataa to be pushed onto the stack
+* Return: no return
+*/
+void pushnode(stack_t **head, int value)
+{
+stack_t *new_node, *curr;
+curr = *head;
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+if (curr)
+curr->prev = new_node;
+new_node->n = value;
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
 }
